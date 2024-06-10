c30103834b82438a3899cad51694baf3638863cc
fatma2705 <arafatma2003@gmail.com>
2024-06-10 17:29:38 +0200

    login component compilato

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { inject } from '@angular/core';
+import { FormControl,FormGroup,ReactiveFormsModule,Validators } from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule,RouterModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+
+  authService = inject(AuthService);
+  router = inject(Router);
+
+  protected loginForm = new FormGroup({
+    email: new FormControl('',[Validators.required,Validators.email]),
+    password: new FormControl('',[Validators.required])
+  })
+
+  onSubmit(){
+    
+  }
 
 }
