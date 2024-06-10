f5d7d3a4a6d35ea29d4dc34f86db7ff202473cd8
fatma2705 <arafatma2003@gmail.com>
2024-06-10 15:34:44 +0200

    'update project setup'

---
+++ src/app/analysis/analysis-detail/analysis-detail.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-analysis-detail',
+  standalone: true,
+  imports: [],
+  templateUrl: './analysis-detail.component.html',
+  styleUrl: './analysis-detail.component.css'
+})
+export class AnalysisDetailComponent {
+
+}
---
+++ src/app/analysis/analysis-list/analysis-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-analysis-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './analysis-list.component.html',
+  styleUrl: './analysis-list.component.css'
+})
+export class AnalysisListComponent {
+
+}
