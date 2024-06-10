6b37c7765a05444d0c46e113b7cd2336c8a030e3
fatma2705 <arafatma2003@gmail.com>
2024-06-10 16:54:14 +0200

    implementazione del auth.service

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { AdminComponent } from './auth/admin/admin.component';
+import { authGuard } from './auth/auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:"", redirectTo:"/login", pathMatch: "full"
+    },
+    {
+        path: 'login', component: LoginComponent
+    },
+    {
+        path: 'register', component: RegisterComponent
+    },
+
+    {
+        path: 'admin', component: AdminComponent, canActivate: [authGuard]
+    }
+];
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { tap } from 'rxjs/operators';
+
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +10,26 @@ import { Injectable } from '@angular/core';
 export class AuthService {
 
   constructor() { }
+
+  httpClient = inject(HttpClient);
+  baseUrl = 'http://localhost:8080/api/auth';
+
+  signup(data: any){
+    return this.httpClient.post('${this.baseUrl}/register',data);
+  }
+
+  login(data: any){
+    return this.httpClient.post('${this.baseUrl}/login',data)
+    .pipe(tap((result) => {
+      localStorage.setItem('authUser',JSON.stringify(result));
+    }));
+  }
+
+  logout(){
+    localStorage.removeItem('authUser');
+  }
+
+  isLoggedIn() {
+    return localStorage.getItem('authUser') !== null;
+  }
 }
